0516d60785a71da0ba6eb4eb295d264776624752
Vinícius Roberto <161877574+Vinicius-Roberto@users.noreply.github.com>
2024-05-19 10:54:40 -0300

    Update modal.js

--- TelaColaborador/js/modal.js
+++ TelaColaborador/js/modal.js
@@ -8,6 +8,13 @@ const toggleModal = () => {
     fade.classList.toggle("hide");
 };
 
-[openModalButton, closeModalButton, fade].forEach((el) => {
-    el.addEventListener("click", toggleModal);
+const redirectAfterClose = () => {
+    window.location.href = "minhascampanhas.html"; // Substitua "sua-url-especifica.html" pela URL que deseja redirecionar
+};
+
+openModalButton.addEventListener("click", toggleModal);
+closeModalButton.addEventListener("click", () => {
+    toggleModal();
+    redirectAfterClose(); // Redireciona o usuário após fechar o modal
 });
+fade.addEventListener("click", toggleModal);
